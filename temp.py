f3d68a366a29026b6c2dd616388ea303376878ce
Sebas1705 <sebssgarcia502580@gmail.com>
2024-04-28 22:51:56 +0200

    pre

--- temp.py
+++ temp.py
@@ -1,165 +1,123 @@
 import cv2
-from matplotlib import pyplot as plt 
-import os
 import numpy as np
 
-def save_images(images_to_save):
-    for i in range(len(images_to_save)):
-        cv2.imwrite(f"./imagenesResultado/{i:0>5}.png",cv2.cvtColor(images_to_save[i],cv2.COLOR_BGR2RGB))
+# Leer la imagen y convertirla a escala de grises
+image = cv2.imread('./proyect/images/test/00003.png')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-def save_images2(images_to_save):
-    for i in range(len(images_to_save)):
-        cv2.imwrite(f"./imagenesfinales/{i:0>5}.png",cv2.cvtColor(images_to_save[i],cv2.COLOR_BGR2RGB))
+# Crear el detector MSER
+mser = cv2.MSER_create()
+
+# Detectar regiones de alto contraste
+regions, _ = mser.detectRegions(gray)
+
+# Inicializar lista para almacenar los rectángulos de las regiones detectadas
+rectangles = []
+
+# Procesar cada región detectada
+for region in regions:
+    # Obtener las coordenadas del rectángulo que envuelve la región
+    x, y, w, h = cv2.boundingRect(region)
+    
+    # Calcular la relación de aspecto
+    aspect_ratio = w / h
+    
+    # Filtrar regiones con relación de aspecto adecuada
+    if 0.5 <= aspect_ratio <= 2.0:
+        # Agrandar el rectángulo para incluir el borde blanco
+        x -= 10
+        y -= 10
+        w += 20
+        h += 20
         
-directory = "./imagenesTest/"
-rgb_images = [cv2.cvtColor(cv2.imread(os.path.join(directory, file)),cv2.COLOR_BGR2RGB) for file in os.listdir(directory)][0:10]
-imgs = [cv2.cvtColor(cv2.imread(os.path.join(directory, file)),cv2.COLOR_BGR2RGB) for file in os.listdir(directory)][0:10]
+        rectangles.append((x, y, w, h))
+
+# Mostrar las regiones detectadas
+for (x, y, w, h) in rectangles:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+cv2.imshow('MSER Regions', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
-plt.subplot(1,2,1)
-plt.imshow(rgb_images[0])
-plt.title("Primera")
+# Convertir la imagen a espacio de color HSV
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-plt.subplot(1,2,2)
-plt.imshow(rgb_images[len(rgb_images)-1])
-plt.title("Ulitma")
+# Definir el rango de color azul saturado en HSV
+lower_blue = np.array([90, 50, 50])
+upper_blue = np.array([130, 255, 255])
 
-gray_images = [cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) for image in rgb_images]
+# Crear la máscara
+mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
-plt.subplot(2,2,1)
-plt.imshow(gray_images[0], cmap="gray")
-plt.title("Primera Antes")
+# Aplicar la máscara a la imagen original
+blue_pixels = cv2.bitwise_and(image, image, mask=mask)
 
-plt.subplot(2,2,2)
-plt.imshow(gray_images[len(rgb_images)-1], cmap="gray")
-plt.title("Ulitma Antes")
+cv2.imshow('Blue Pixels', blue_pixels)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-for i in range(len(gray_images)):
-    gray_images[i] = cv2.equalizeHist(gray_images[i])
+# # Definir la máscara de color azul saturado ideal
+# ideal_mask = np.ones((40, 80), dtype=np.uint8) * 255
 
-plt.subplot(2,2,3)
-plt.imshow(gray_images[0], cmap="gray")
-plt.title("Primera Despues")
+# # Inicializar la lista para almacenar los scores de las ventanas detectadas
+# scores = []
 
-plt.subplot(2,2,4)
-plt.imshow(gray_images[len(rgb_images)-1], cmap="gray")
-plt.title("Ulitma Despues")
+# # Procesar cada región detectada por MSER
+# for (x, y, w, h) in rectangles:
+#     # Recortar la región detectada y cambiar su tamaño
+#     region_resized = cv2.resize(gray[y:y+h, x:x+w], (80, 40))
+    
+#     # Extraer la máscara de color azul saturado
+#     blue_mask = cv2.inRange(cv2.cvtColor(cv2.resize(image[y:y+h, x:x+w], (80, 40)), cv2.COLOR_BGR2HSV), lower_blue, upper_blue)
+    
+#     # Correlar la máscara con la máscara ideal
+#     correlation = cv2.matchTemplate(blue_mask, ideal_mask, cv2.TM_CCOEFF_NORMED)[0][0]
+    
+#     # Establecer el score como la correlación
+#     scores.append(correlation)
 
-mser = cv2.MSER_create(delta=4, min_area=1000, max_area=80000, max_variation=0.9, min_diversity=0.1)
+# # Mostrar los scores
+# print(scores)
 
-def getRegions(grays):
-    allRegions = []
-    for gray in grays:
-        _, boundingBoxes = mser.detectRegions(gray)
-        allRegions.append(boundingBoxes)
-    return allRegions
+# Definir la función para calcular el índice de solapamiento entre dos rectángulos
+def overlap_ratio(rect1, rect2):
+    x1 = max(rect1[0], rect2[0])
+    y1 = max(rect1[1], rect2[1])
+    x2 = min(rect1[0] + rect1[2], rect2[0] + rect2[2])
+    y2 = min(rect1[1] + rect1[3], rect2[1] + rect2[3])
+    
+    intersection_area = max(0, x2 - x1) * max(0, y2 - y1)
+    union_area = rect1[2] * rect1[3] + rect2[2] * rect2[3] - intersection_area
+    
+    overlap_ratio = intersection_area / union_area
+    return overlap_ratio
 
-def groupRegions(list_boxes):
-    groupped_regions = []
-    for boxes in list_boxes:
-        groupped_rects, _ = cv2.groupRectangles(boxes, 1, eps=0.07)
-        groupped_regions.append(groupped_rects)        
-    return groupped_regions
+# Definir un umbral de solapamiento
+overlap_threshold = 0.5
 
-def drawRegions(images_list, all_regions):   
-    groupped_regions = []
-    i = 0
-    for img, regs in zip(images_list, all_regions):   
-        for region in regs:            
-            x, y, w, h = region
-            aspect_ratio = float(w) / h
-            if 0.5 < aspect_ratio < 4:
-                if w < 500:
-                    if h < 500:
-                        area = h * w
-                        cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
-                        groupped_regions.append((region,i)) 
-        i = i +1  
-    return groupped_regions  
+# Inicializar lista para almacenar las detecciones filtradas
+filtered_rectangles = []
 
+# Iterar sobre todas las ventanas y seleccionar una única ventana por cada sub-panel
+for i, rect1 in enumerate(rectangles):
+    if i not in filtered_rectangles:
+        for j, rect2 in enumerate(rectangles):
+            if i != j and overlap_ratio(rect1, rect2) > overlap_threshold:
+                if rect1[2] * rect1[3] > rect2[2] * rect2[3]:
+                    filtered_rectangles.append(j)
+                else:
+                    filtered_rectangles.append(i)
 
-dectected_regions = drawRegions(rgb_images, groupRegions(getRegions(gray_images)))
+# Eliminar las detecciones duplicadas
+filtered_rectangles = list(set(range(len(rectangles))) - set(filtered_rectangles))
 
-plt.subplot(1,2,1)
-plt.imshow(rgb_images[0])
-plt.title("Primera")
+# Mostrar las detecciones filtradas
+for idx in filtered_rectangles:
+    rect = rectangles[idx]
+    cv2.rectangle(image, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 2)
 
-plt.subplot(1,2,2)
-plt.imshow(rgb_images[len(rgb_images)-1])
-plt.title("Ulitma")
-
-# save_images(rgb_images)
-
-recortes = []
-for region in dectected_regions:    
-    x, y, w, h = region[0]    
-    cropped_image = rgb_images[region[1]][y:y+h, x:x+w]
-    recortes.append((cropped_image,region[1],region[0]))
-
-plt.subplot(1,2,1)
-plt.imshow(recortes[len(recortes)-1][0])
-plt.title("Primera")
-
-plt.subplot(1,2,2)
-plt.imshow(rgb_images[recortes[len(recortes)-1][1]])
-plt.title("Imagen")
-
-hsv_images = [cv2.cvtColor(im, cv2.COLOR_BGR2HSV) for im in rgb_images]
-
-rgb_recortes = [cv2.cvtColor(img, cv2.COLOR_BGR2RGB) for img,i,r in recortes]
-hsv_recortes = [cv2.cvtColor(im, cv2.COLOR_BGR2HSV) for im in rgb_recortes]
-
-lower_blue = np.array([100,50,60])
-upper_blue = np.array([130,255,255])
-
-def apply_mask(img, lower_blue, upper_blue):
-    mask_aux  = cv2.inRange(img, lower_blue, upper_blue)
-    return cv2.bitwise_and(img, img, mask=mask_aux)
-
-def save_recortes(images_with_index,mask: bool):
-    for i in range(len(images_with_index)):
-        cv2.imwrite(f"./recortes/{i:0>5}-{images_with_index[i][1]}{"" if mask else "-noMask"}.png",cv2.cvtColor(images_with_index[i][0],cv2.COLOR_BGR2RGB))   
-
-
-plt.subplot(2,2,1)
-plt.imshow(recortes[46][0])
-plt.subplot(2,2,2)
-plt.imshow(rgb_images[3])
-plt.subplot(2,2,3)
-plt.imshow(cv2.cvtColor(cv2.cvtColor(apply_mask(hsv_recortes[46], lower_blue, upper_blue), cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2RGB))
-plt.subplot(2,2,4)
-plt.imshow(cv2.cvtColor(cv2.cvtColor(apply_mask(hsv_images[3], lower_blue, upper_blue), cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2RGB))            
-
-
-recortes_mascara = []
-for rec in hsv_recortes:
-    recortes_mascara.append(cv2.cvtColor(cv2.cvtColor(apply_mask(rec, lower_blue, upper_blue), cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2RGB))
-
-
-imgs_noMask = []
-imgs_mask = []
-region_blue = []
-for i in range(len(recortes_mascara)):
-    gris = cv2.cvtColor(recortes_mascara[i], cv2.COLOR_BGR2GRAY)
-    # Contar los píxeles negros (píxeles con valor 0)
-    total_pixeles = gris.shape[0] * gris.shape[1]
-    pxeles_negros = total_pixeles - cv2.countNonZero(gris)
-    # Calcular el porcentaje de píxeles negros
-    porcentaje = (pxeles_negros / total_pixeles) * 100
-    if porcentaje < 14:
-        imgs_noMask.append(recortes[i])
-        imgs_mask.append((recortes_mascara[i],recortes[i][1]))
-        region_blue.append((recortes[i][2],recortes[i][1]))
-
-print(region_blue)
-save_recortes(imgs_noMask, False)
-save_recortes(imgs_mask, True)
-
-def draw_final_region(region,img):
-        for reg,i in region:
-            x, y, w, h = reg
-            cv2.rectangle(img[i], (x,y), (x+w, y+h), (0,255,0), 2)            
-
-
-draw_final_region(region_blue,imgs)
-save_images2(imgs)
+cv2.imshow('Filtered MSER Regions', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
